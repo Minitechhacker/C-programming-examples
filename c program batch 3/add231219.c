133879d9da11ad0f68f17ef1564d7e960825038f
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2019-12-23 19:38:26 +0530

    Add files via upload

---
+++ c program batch 3/add231219.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("what is your  values:");
+	scanf("%d",&a);
+	
+	printf("what is your  values:");
+	scanf("%d",&b);
+	
+	c=a+b;
+	c++;
+	printf("\n your resluts = %d",c);
+	c=a-b;
+	printf("\n your resluts = %d",c);
+}
