e352784cc7e903721f8f1d874a922b861f99dc31
Mini_tech_hacker <57276723+Minitechhacker@users.noreply.github.com>
2020-01-06 19:11:18 +0530

    Add files via upload

---
+++ c batch weekend/runtimeadd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("what is your add values:");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	c++;
+	++c;
+	printf("your add values:%d",c);
+}
